7d2c2923c09c5f1c0335c45be369c593075e7aa6
mdan22 <mdan22@proton.me>
2024-09-14 20:18:30 +0200

    add solution for 4.3

---
+++ utils/list_helper.js
@@ -0,0 +1,13 @@
+// list_helper.js is a collection of helper functions for
+// working with the describe sections of the blog list
+
+// function that receives an array of blog posts as a parameter and always returns the value 1
+const dummy = (blogs) => {
+  return 1
+}
+
+const list_helper = {
+  dummy
+}
+
+module.exports = list_helper
