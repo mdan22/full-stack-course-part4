c81442500eea38c02df13c0e67ea666f1d3b6485
mdan22 <mdan22@proton.me>
2024-09-15 14:52:58 +0200

    add solution for 4.5*

--- test/favoriteLikes.test.js
+++ test/favoriteLikes.test.js
@@ -1,4 +1,4 @@
-// totalLikes.test.js contains tests for function 'totalLikes'
+// favoriteLikes.test.js contains tests for function 'favoriteLikes'
 // tests are written using Node.js's built-in assert module
 
 const { test, describe } = require('node:test')
@@ -6,7 +6,7 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
 // using a describe block to group tests into a logical collection
-describe('total likes', () => {
+describe('favorite likes', () => {
   const emptyList = []
 
   const listWithOneBlog = [
@@ -33,7 +33,7 @@ describe('total likes', () => {
       'title': 'Hello, Blog!',
       'author': 'Akira Taguchi',
       'url': 'https://akirataguchi115.github.io/misc/2023/12/31/hello-blog.html',
-      'likes': 3
+      'likes': 6
     },
     {
       'title': 'How to install Windows',
@@ -43,18 +43,34 @@ describe('total likes', () => {
     },
   ]
 
-  test('of empty list is zero', () => {
-    const result = listHelper.totalLikes(emptyList)
-    assert.strictEqual( result, 0)
+  test('of empty list is also an empty blog list', () => {
+    const result = listHelper.favoriteLikes(emptyList)
+    assert.deepStrictEqual( result, {})
   })
 
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    assert.strictEqual( result, 5)
+  test('when list has only one blog, equals that blog', () => {
+    const result = listHelper.favoriteLikes(listWithOneBlog)
+    assert.deepStrictEqual( result,
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+      }
+    )
   })
 
-  test('of bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(biggerList)
-    assert.strictEqual( result, 15)
+  test('of bigger list is finding the right blog', () => {
+    const result = listHelper.favoriteLikes(biggerList)
+    assert.deepStrictEqual( result,
+      {
+        'title': 'How to install Windows',
+        'author': 'Akira Taguchi',
+        'url': 'https://akirataguchi115.github.io/misc/2024/01/14/how-to-install-windows.html',
+        'likes': 7
+      }
+    )
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,8 @@
 // list_helper.js is a collection of helper functions for
 // working with the describe sections of the blog list
 
-// function that receives an array of blog posts as a parameter and always returns the value 1
+// function that receives an array of blog posts
+// and returns the total sum of likes of all blog posts
 const totalLikes = (blogs) => {
   const reducer = (sum, blogs) => {
     return sum + blogs.likes
@@ -12,8 +13,23 @@ const totalLikes = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 
+// function that receives an array of blog posts
+// and returns the blog post with the most likes
+const favoriteLikes = (blogs) => {
+  // reducer checks for each blog if it has more likes
+  // if it does it updates the current favorite blog
+  const reducer = (favorite, blog) => {
+    return (blog.likes > favorite.likes) ? blog : favorite
+  }
+
+  return blogs.length === 0
+    ? {}
+    : blogs.reduce(reducer, blogs[0])
+}
+
 const list_helper = {
-  totalLikes
+  totalLikes,
+  favoriteLikes
 }
 
 module.exports = list_helper
