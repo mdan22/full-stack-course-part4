f8fa0423085e54b7f6bee90285d599bdd1c7c808
mdan22 <mdan22@proton.me>
2024-09-21 16:45:52 +0200

    add solution for 4.10

--- test/blog_api.test.js
+++ test/blog_api.test.js
@@ -30,6 +30,7 @@ beforeEach(async () => {
 // Verify that the blog list application returns the
 // correct amount of blog posts in the JSON format
 test('all blogs are returned correctly and in the json format', async () => {
+  // make the HTTP GET request
   const response =
   await api
     .get('/api/blogs')
@@ -38,6 +39,8 @@ test('all blogs are returned correctly and in the json format', async () => {
 
   // execution gets here only after the HTTP request is complete
   // the result of HTTP request is saved in variable response
+
+  // verify that the total number of blogs in system and db is the same and correct
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
@@ -46,15 +49,44 @@ test('all blogs are returned correctly and in the json format', async () => {
 // blog posts is named id, by default the database
 // names the property _id
 test('all blogs have a property named id, but not _id', async () => {
+  // make the HTTP GET request
   const response =
   await api
     .get('/api/blogs')
 
+  // verify that all blogs have the property id
   const blogs = response.body
   blogs.forEach(blog => {
     assert(blog.id)
     assert(!blog._id) // could be skipped but done for clarity
   })
+})
+
+// 4.10: Blog List Test, setp 3
+// Verify that making a HTTP POST request for the
+// /api/blogs URL successfully creates a new blog post
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    'title': 'How to install Windows',
+    'author': 'Akira Taguchi',
+    'url': 'https://akirataguchi115.github.io/misc/2024/01/14/how-to-install-windows.html',
+    'likes': 7
+  }
+
+  // make the HTTP POST request
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201) // content created
+    .expect('Content-Type', /application\/json/)
+
+  // verify that the total number of blogs in the system is increased by one
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  // verify that the added blog is in DB
+  const titles = blogsAtEnd.map(blog => blog.title)
+  assert(titles.includes('How to install Windows'))
 })
 
 after(async () => {
