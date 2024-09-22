fc66a8e57e2e45904ee6f37128a0faa9fa45e5d9
mdan22 <mdan22@proton.me>
2024-09-22 09:37:47 +0200

    add solution for 4.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const mongoose = require('mongoose')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -20,6 +21,22 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
 
   response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+
+  // Check if the id is a valid MongoDB ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    // Respond with status code 400: Not Found if id is invalid
+    return response.status(400).json({ error: 'malformatted id' })
+  }
+
+  // Attempt to delete the blog
+  await Blog.findByIdAndDelete(request.params.id)
+
+  // Respond with 204 No Content whether the blog existed or not
+  response.status(204).end() // No Content (id is valid + exists or not)
 })
 
 module.exports = blogsRouter
--- test/blog_api.test.js
+++ test/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 
 const supertest = require('supertest')
@@ -15,147 +15,192 @@ const Blog = require('../models/blog')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  console.log('cleared')
-
-  for(let blog of helper.initialBlogs) {
-    let blogObject = new Blog(blog)
-    await blogObject.save()
-    console.log('saved')
-  }
-
-  console.log('done')
+  // insertMany instead of for of loop to make code simpler
+  await Blog.insertMany(helper.initialBlogs)
 })
 
-// 4.8: Blog List Tests, step 1
-// Verify that the blog list application returns the
-// correct amount of blog posts in the JSON format
-test('all blogs are returned correctly and in the json format', async () => {
-  // make the HTTP GET request
-  const response =
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved', async () => {
+  // 4.8: Blog List Tests, step 1
+  // Verify that the blog list application returns the
+  // correct amount of blog posts in the JSON format
+  test('all blogs are returned correctly and in the json format', async () => {
+    // make the HTTP GET request
+    const response =
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  // execution gets here only after the HTTP request is complete
-  // the result of HTTP request is saved in variable response
+    // execution gets here only after the HTTP request is complete
+    // the result of HTTP request is saved in variable response
 
-  // verify that the total number of blogs in system and db is the same and correct
-  assert.strictEqual(response.body.length, helper.initialBlogs.length)
-})
-
-// 4.9: Blog List Tests, step 2
-// Verify that the unique identifier property of the
-// blog posts is named id, by default the database
-// names the property _id
-test('all blogs have a property named id, but not _id', async () => {
-  // make the HTTP GET request
-  const response =
-  await api
-    .get('/api/blogs')
-
-  // verify that all blogs have the property id
-  const blogs = response.body
-  blogs.forEach(blog => {
-    assert(blog.id)
-    assert(!blog._id) // could be skipped but done for clarity
+    // verify that the total number of blogs in system and db is the same and correct
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
-})
 
-// 4.10: Blog List Test, setp 3
-// Verify that making a HTTP POST request for the
-// /api/blogs URL successfully creates a new blog post
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    'title': 'How to install Windows',
-    'author': 'Akira Taguchi',
-    'url': 'https://akirataguchi115.github.io/misc/2024/01/14/how-to-install-windows.html',
-    'likes': 7
-  }
+  // 4.9: Blog List Tests, step 2
+  // Verify that the unique identifier property of the
+  // blog posts is named id, by default the database
+  // names the property _id
+  test('all blogs have a property named id, but not _id', async () => {
+    // make the HTTP GET request
+    const response =
+    await api
+      .get('/api/blogs')
 
-  // make the HTTP POST request
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201) // content created
-    .expect('Content-Type', /application\/json/)
+    // verify that all blogs have the property id
+    const blogs = response.body
+    blogs.forEach(blog => {
+      assert(blog.id)
+      assert(!blog._id) // could be skipped but done for clarity
+    })
+  })
 
-  // verify that the total number of blogs in the system
-  // is increased by one
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+  describe('viewing a specific blog', () => {
+    // more tests can be added later
+  })
 
-  // verify that the added blog is in DB
-  const titles = blogsAtEnd.map(blog => blog.title)
-  assert(titles.includes('How to install Windows'))
-})
+  describe('addition of a new blog', () => {
+    // 4.10: Blog List Test, step 3
+    // Verify that making a HTTP POST request for the
+    // /api/blogs URL successfully creates a new blog post
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        'title': 'How to install Windows',
+        'author': 'Akira Taguchi',
+        'url': 'https://akirataguchi115.github.io/misc/2024/01/14/how-to-install-windows.html',
+        'likes': 7
+      }
 
-// 4.11*: Blog List Tests, step 4
-// Verify that if the likes property is missing from
-// the request, it will default to the value zero
-test('like property missing results in value 0', async () => {
-  const newBlog =     {
-    'title': 'Hello, Blog!',
-    'author': 'Akira Taguchi',
-    'url': 'https://akirataguchi115.github.io/misc/2023/12/31/hello-blog.html'
-  }
+      // make the HTTP POST request
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201) // content created
+        .expect('Content-Type', /application\/json/)
 
-  // make the HTTP POST request
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201) // content created
-    .expect('Content-Type', /application\/json/)
+      // verify that the total number of blogs in the system
+      // is increased by one
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-  // verify that the total number of blogs in the system
-  // is increased by one
-  const blogsAtEnd = await helper.blogsInDb()
-  assert.deepStrictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+      // verify that the added blog is in DB
+      const titles = blogsAtEnd.map(blog => blog.title)
+      assert(titles.includes('How to install Windows'))
+    })
 
-  // verify that the added blog is in DB
-  const titles = blogsAtEnd.map(blog => blog.title)
-  assert(titles.includes('Hello, Blog!'))
+    // 4.11*: Blog List Tests, step 4
+    // Verify that if the likes property is missing from
+    // the request, it will default to the value zero
+    test('like property missing results in value 0', async () => {
+      const newBlog =     {
+        'title': 'Hello, Blog!',
+        'author': 'Akira Taguchi',
+        'url': 'https://akirataguchi115.github.io/misc/2023/12/31/hello-blog.html'
+      }
 
-  // verify that the likes property is set to 0
-  assert.strictEqual(blogsAtEnd[blogsAtEnd.length - 1].likes, 0)
-})
+      // make the HTTP POST request
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201) // content created
+        .expect('Content-Type', /application\/json/)
 
-// 4.12*: Blog List tests, step 5
-// Verify that if the title or url properties are missing
-// from the request data, the backend responds with the
-// status code 400 Bad Request
-test('title or url properties missing result in status code 400 Bad Request', async () => {
-  const newBlogNoTitle =     {
-    'author': 'Akira Taguchi',
-    'url': 'https://akirataguchi115.github.io/misc/2023/12/31/hello-blog.html'
-  }
+      // verify that the total number of blogs in the system
+      // is increased by one
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.deepStrictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-  const newBlogNoUrl =     {
-    'title': 'Hello, Blog!',
-    'author': 'Akira Taguchi',
-  }
+      // verify that the added blog is in DB
+      const titles = blogsAtEnd.map(blog => blog.title)
+      assert(titles.includes('Hello, Blog!'))
 
-  const newBlogOnlyAuthor =     {
-    'author': 'Akira Taguchi',
-  }
+      // verify that the likes property is set to 0
+      assert.strictEqual(blogsAtEnd[blogsAtEnd.length - 1].likes, 0)
+    })
 
-  // make HTTP POST request without title
-  await api
-    .post('/api/blogs')
-    .send(newBlogNoTitle)
-    .expect(400) // bad request
+    // 4.12*: Blog List tests, step 5
+    // Verify that if the title or url properties are missing
+    // from the request data, the backend responds with the
+    // status code 400 Bad Request
+    test('title or url properties missing result in status code 400 Bad Request', async () => {
+      const newBlogNoTitle =     {
+        'author': 'Akira Taguchi',
+        'url': 'https://akirataguchi115.github.io/misc/2023/12/31/hello-blog.html'
+      }
 
-  // make HTTP POST request without url
-  await api
-    .post('/api/blogs')
-    .send(newBlogNoUrl)
-    .expect(400) // bad request
+      const newBlogNoUrl =     {
+        'title': 'Hello, Blog!',
+        'author': 'Akira Taguchi',
+      }
 
-  // make HTTP POST request with missing title and url
-  await api
-    .post('/api/blogs')
-    .send(newBlogOnlyAuthor)
-    .expect(400) // bad request
+      const newBlogOnlyAuthor =     {
+        'author': 'Akira Taguchi',
+      }
+
+      // make HTTP POST request without title
+      await api
+        .post('/api/blogs')
+        .send(newBlogNoTitle)
+        .expect(400) // bad request
+
+      // make HTTP POST request without url
+      await api
+        .post('/api/blogs')
+        .send(newBlogNoUrl)
+        .expect(400) // bad request
+
+      // make HTTP POST request with missing title and url
+      await api
+        .post('/api/blogs')
+        .send(newBlogOnlyAuthor)
+        .expect(400) // bad request
+    })
+  })
+
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid and exists', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      // using notesAtStart here (instead of helper.initialNotes) so the test dynamically adapts to the current state of the database
+      assert.strictEqual(blogsAtStart.length - 1, blogsAtEnd.length)
+
+      const titles = blogsAtEnd.map(b => b.title)
+      assert(!titles.includes(blogToDelete.title))
+    })
+
+    test('results in status code 400 if id is invalid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const invalidId = '5a3d5da59070081a82a3445'
+
+      await api
+        .delete(`/api/blogs/${invalidId}`)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      // using notesAtStart here (instead of helper.initialNotes) so the test dynamically adapts to the current state of the database
+      assert.strictEqual(blogsAtStart.length, blogsAtEnd.length)
+    })
+
+    test('results in status code 204 if id is valid but blog doesn\'t exist', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const validNonExistentId = '66efbe933f0982d6deec6924'
+
+      await api
+        .delete(`/api/blogs/${validNonExistentId}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      // using notesAtStart here (instead of helper.initialNotes) so the test dynamically adapts to the current state of the database
+      assert.strictEqual(blogsAtStart.length, blogsAtEnd.length)
+    })
+  })
 })
 
 after(async () => {
