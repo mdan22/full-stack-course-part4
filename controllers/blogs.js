c7da8bdba70a4978c24285f5f6524a3750464888
mdan22 <mdan22@proton.me>
2024-09-25 10:07:28 +0200

    remove if blocks in 'blogs.js' which are redundant

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,7 +2,6 @@
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const mongoose = require('mongoose')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -13,25 +12,12 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  // handle missing title or url for 4.12*
-  if (!blog.title || !blog.url ) {
-    response.status(400).end()
-  }
-
   const savedBlog = await blog.save()
 
   response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const id = request.params.id
-
-  // Check if the id is a valid MongoDB ObjectId
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    // Respond with status code 400: Not Found if id is invalid
-    return response.status(400).json({ error: 'malformatted id' })
-  }
-
   // Attempt to delete the blog
   await Blog.findByIdAndDelete(request.params.id)
 
@@ -41,11 +27,6 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.put('/:id', async (request, response) => {
   const { title, author, url, likes } = request.body
-
-  // Check if the id is a valid MongoDB ObjectId
-  if (!mongoose.Types.ObjectId.isValid(request.params.id)) {
-    return response.status(400).json({ error: 'malformatted id' }) // Bad Request
-  }
 
   const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
